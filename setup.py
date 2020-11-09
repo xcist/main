23c1a50cf7593955b8c9f5d3ae9e71f861df820d
MingyeWu <Mingye.Wu@ge.com>
2020-11-09 17:52:29 -0500

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup
 
 setup(name='catsim',
-      version='0.1.1',
+      version='0.1.2',
       description='Simulation toolkit for X-ray based cancer imaging',
       url='https://github.com/xcist',
       author='Mingye Wu, Paul FitzGerald, Brion Sarachan, Bruno De Man',
@@ -16,4 +16,4 @@ setup(name='catsim',
       packages=['catsim'],
       zip_safe=False,
       package_data={'catsim':[r'lib/*.*', r'cfg/*.cfg', r'data/bowtie/*.dat', r'data/material/*', r'data/phantom/*.*', r'data/spectrum/*.dat', r'data/material/edlp/*/*.dat', ]},
-      include_package_data=False)
+      include_package_data=True)
