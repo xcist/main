b1f0e782fcf8a9287ecb111bb9b01a7cfd946ddd
Jiayong Zhang <jiayong.zhang@ge.com>
2022-04-22 10:38:13 -0400

    comment unused functions

--- catsim/Phantom_Analytic.py
+++ catsim/Phantom_Analytic.py
@@ -799,125 +799,125 @@ def Phantom_Analytic_SetObjects(objs=None, debug=False):
     return phantObject, numObjects, T, cumCP, NumCP, materialIndex, X, K, Q, Eta, S, D
 
 # NOTE:not verified yet
-def Phantom_Analytic_pp_to_ppm(PhantomFileBasename, Scale=1., debug=False):
-
-    print('Converting phantom file {}.pp to .ppm file...'.format(PhantomFileBasename))
-
-    ppPhantomFilename = PhantomFileBasename + '.pp'
-    tmpPhantomFilename = PhantomFileBasename + '.tmp'
-
-    #TODO: need to change this to python wrapper
-    feval('C_Phantom_Analytic_FORBILD_to_tmp',Scale,ppPhantomFilename,tmpPhantomFilename)
-
-    print('Reading and deleting {}.'.format(tmpPhantomFilename))
-
-    with open(tmpPhantomFilename, 'r') as f:
-        lns = f.readlines()
-
-    os.remove(tmpPhantomFilename)
-
-    ppmPhantomFilename = PhantomFileBasename + '.ppm'
-    print('Writing {}.'.format(ppmPhantomFilename))
-
-    f = open(ppmPhantomFilename, 'w')
-    ind = [i for i in range(len(lns)) if '35' in lns[i, 1]]
-    if len(ind) < 2:
-        sys.exit('material table not found')
-    else:
-        f.write('materialList = {')
-        for i in range(1,ind(2) - 2):
-            f.write('\'%s\' ', lns[i + 1])
-        f.write('};\n\n')
-        offset = ind[2]
+#def Phantom_Analytic_pp_to_ppm(PhantomFileBasename, Scale=1., debug=False):
+#
+#    print('Converting phantom file {}.pp to .ppm file...'.format(PhantomFileBasename))
+#
+#    ppPhantomFilename = PhantomFileBasename + '.pp'
+#    tmpPhantomFilename = PhantomFileBasename + '.tmp'
+#
+#    #TODO: need to change this to python wrapper
+#    feval('C_Phantom_Analytic_FORBILD_to_tmp',Scale,ppPhantomFilename,tmpPhantomFilename)
+#
+#    print('Reading and deleting {}.'.format(tmpPhantomFilename))
+#
+#    with open(tmpPhantomFilename, 'r') as f:
+#        lns = f.readlines()
+#
+#    os.remove(tmpPhantomFilename)
+#
+#    ppmPhantomFilename = PhantomFileBasename + '.ppm'
+#    print('Writing {}.'.format(ppmPhantomFilename))
+#
+#    f = open(ppmPhantomFilename, 'w')
+#    ind = [i for i in range(len(lns)) if '35' in lns[i, 1]]
+#    if len(ind) < 2:
+#        sys.exit('material table not found')
+#    else:
+#        f.write('materialList = {')
+#        for i in range(1,ind(2) - 2):
+#            f.write('\'%s\' ', lns[i + 1])
+#        f.write('};\n\n')
+#        offset = ind[2]
+#    
+#    i=0
+#    while (offset < size(lns,1) - 15):
+#        i += 1
+#        tmpo,tmpc,lns,offset,clip = read_object(lns, offset)
+#        # TODO: first save string then flush all at once
+#        f.write('object.center({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.cent(1),tmpo.cent(2),tmpo.cent(3)));
+#        f.write('object.half_axes({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.hax(1),tmpo.hax(2),tmpo.hax(3)));
+#        f.write('object.euler_angs({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.ea(1),tmpo.ea(2),tmpo.ea(3)));
+#        f.write('object.density({%d}) = {%f};\\n'.format(i,tmpo.density))
+#        f.write('object.type({%d}) = {%d};\\n'.format(i,tmpo.type))
+#        f.write('object.material({%d}) = {%d};\\n'.format(i,tmpo.material))
+#        f.write('object.axial_lims({%d},:) = [0 0];\\n'.format(i))
+#        f.write('object.shape(%d) = 0;\\n',i)
+#        f.write('object.clip{%d} = ['.format(i))
+#        for j in range(1,tmpc.shape[0]):
+#            f.write('{%f} {%f} {%f} {%f};'.format(tmpc(j,1),tmpc(j,2),tmpc(j,3),tmpc(j,4)))
+#        f.write('];\\n\\n')
+#        clip[i]=tmpc
+#        obj[i]=tmpo
+#    
+#    f.close()
+#    print('... done writing {}'.format(ppmPhantomFilename))
     
-    i=0
-    while (offset < size(lns,1) - 15):
-        i += 1
-        tmpo,tmpc,lns,offset,clip = read_object(lns, offset)
-        # TODO: first save string then flush all at once
-        f.write('object.center({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.cent(1),tmpo.cent(2),tmpo.cent(3)));
-        f.write('object.half_axes({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.hax(1),tmpo.hax(2),tmpo.hax(3)));
-        f.write('object.euler_angs({%d},:) = [{%f} {%f} {%f}];\n'.format(i,tmpo.ea(1),tmpo.ea(2),tmpo.ea(3)));
-        f.write('object.density({%d}) = {%f};\\n'.format(i,tmpo.density))
-        f.write('object.type({%d}) = {%d};\\n'.format(i,tmpo.type))
-        f.write('object.material({%d}) = {%d};\\n'.format(i,tmpo.material))
-        f.write('object.axial_lims({%d},:) = [0 0];\\n'.format(i))
-        f.write('object.shape(%d) = 0;\\n',i)
-        f.write('object.clip{%d} = ['.format(i))
-        for j in range(1,tmpc.shape[0]):
-            f.write('{%f} {%f} {%f} {%f};'.format(tmpc(j,1),tmpc(j,2),tmpc(j,3),tmpc(j,4)))
-        f.write('];\\n\\n')
-        clip[i]=tmpc
-        obj[i]=tmpo
-    
-    f.close()
-    print('... done writing {}'.format(ppmPhantomFilename))
-    
-def read_object(lines=None,offset=None):
-
-    obj.type = lines[1 + offset]
-    for i in range(4):
-        A[i] = float(lines[i + 4 + offset])
-    
-    if np.linalg.norm(A - [0,0,0,1]) > 1e-05:
-        sys.exit('Unexpected transform')
-    
-    obj.cent = np.copy(A[:3,3].T)
-    B = A[0:3, 0:3]
-    obj.hax = np.sqrt(np.sum(B*B))
-    B /= np.ones(3,1)*obj.hax
-    B = B.T
-    obj.ea = euler_angs(B)*180/pi
-    obj.transform = copy(A)
-    tmp=sscanf(lines(15 + offset,arange()),'%s',1)
-    tmp = lines[15+offset]
-    #NOTE: we can step matlab code and compare python code
-    if tmp(1) == 'C':
-        i=1
-        while tmp(1) == 'C':
-            v = list(map(float, lines[16+offset, 21:]))
-            clip[i,0:3] = v
-            clip[i,4] = float(lines[16 + offset, 26:])
-            clip[i,:] = -clip(i)
-            offset += 3
-            tmp = lines[15+offset]
-            i += 1
-    else:
-        clip=[]
-    
-    if np.sum((obj.type[1:3]) == 'Cyl') == 3:
-        obj.type = 2
-        obj.hax[3]=obj.hax(3) / 2
-    else:
-        if sum(obj.type[:3] == 'Cub') == 3:
-            obj.type = 2
-            old=0
-            if old:
-                Bt=copy(B)
-            else:
-                Bt=B.T
-
-            clip = clip + [Bt[:,1].T, obj.hax(1)/2 + obj.cent*Bt[:,1]]
-            clip = clip + [-Bt[:,1].T, obj.hax(1)/2 - obj.cent*Bt[:,1]]
-            clip = clip + [Bt[:,2].T, obj.hax(2)/2 + obj.cent*Bt[:,2]]
-            clip = clip + [-Bt[:,2].T, obj.hax(2)/2 - obj.cent*Bt[:,2]]
-            obj.hax[:2] = obj.hax[:2]*np.sqrt(2)
-            obj.hax /= 2
-        else:
-            if np.sum((obj.type[:3]) == 'Sph') == 3:
-                obj.type = 1
-            else:
-                if np.sum(obj.type[:3] == 'Ell') == 3:
-                    obj_type = 1
-                else:
-                    sys.exit('Unrecognized obj type\\n\\r')
-    
-    
-    obj.material = lines[18 + offset]
-    obj.density = lines[19 + offset]
-    offset += 21
-
-    return obj, clip, lines, offset
+#def read_object(lines=None,offset=None):
+#
+#    obj.type = lines[1 + offset]
+#    for i in range(4):
+#        A[i] = float(lines[i + 4 + offset])
+#    
+#    if np.linalg.norm(A - [0,0,0,1]) > 1e-05:
+#        sys.exit('Unexpected transform')
+#    
+#    obj.cent = np.copy(A[:3,3].T)
+#    B = A[0:3, 0:3]
+#    obj.hax = np.sqrt(np.sum(B*B))
+#    B /= np.ones(3,1)*obj.hax
+#    B = B.T
+#    obj.ea = euler_angs(B)*180/pi
+#    obj.transform = copy(A)
+#    tmp=sscanf(lines(15 + offset,arange()),'%s',1)
+#    tmp = lines[15+offset]
+#    #NOTE: we can step matlab code and compare python code
+#    if tmp(1) == 'C':
+#        i=1
+#        while tmp(1) == 'C':
+#            v = list(map(float, lines[16+offset, 21:]))
+#            clip[i,0:3] = v
+#            clip[i,4] = float(lines[16 + offset, 26:])
+#            clip[i,:] = -clip(i)
+#            offset += 3
+#            tmp = lines[15+offset]
+#            i += 1
+#    else:
+#        clip=[]
+#    
+#    if np.sum((obj.type[1:3]) == 'Cyl') == 3:
+#        obj.type = 2
+#        obj.hax[3]=obj.hax(3) / 2
+#    else:
+#        if sum(obj.type[:3] == 'Cub') == 3:
+#            obj.type = 2
+#            old=0
+#            if old:
+#                Bt=copy(B)
+#            else:
+#                Bt=B.T
+#
+#            clip = clip + [Bt[:,1].T, obj.hax(1)/2 + obj.cent*Bt[:,1]]
+#            clip = clip + [-Bt[:,1].T, obj.hax(1)/2 - obj.cent*Bt[:,1]]
+#            clip = clip + [Bt[:,2].T, obj.hax(2)/2 + obj.cent*Bt[:,2]]
+#            clip = clip + [-Bt[:,2].T, obj.hax(2)/2 - obj.cent*Bt[:,2]]
+#            obj.hax[:2] = obj.hax[:2]*np.sqrt(2)
+#            obj.hax /= 2
+#        else:
+#            if np.sum((obj.type[:3]) == 'Sph') == 3:
+#                obj.type = 1
+#            else:
+#                if np.sum(obj.type[:3] == 'Ell') == 3:
+#                    obj_type = 1
+#                else:
+#                    sys.exit('Unrecognized obj type\\n\\r')
+#    
+#    
+#    obj.material = lines[18 + offset]
+#    obj.density = lines[19 + offset]
+#    offset += 21
+#
+#    return obj, clip, lines, offset
     
 def euler_angs(R=None):
 
