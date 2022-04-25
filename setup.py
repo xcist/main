df987137eb78bc39908e1b100fec3467d68f2651
MingyeWu <Mingye.Wu@ge.com>
2022-04-25 16:16:20 -0400

    changed bowtie ext name to txt

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='catsim',
       packages=['catsim'],
       zip_safe=False,
       package_data={'catsim':[r'lib/*.*', r'cfg/*.cfg', 
-        r'data/bowtie/*.dat', r'data/material/*', r'data/material/edlp/*/*.dat', 
+        r'data/bowtie/*.txt', r'data/material/*', r'data/material/edlp/*/*.dat', 
         r'data/phantom/*.*', r'data/scatter/*.dat', r'data/spectrum/*.dat']},
       include_package_data=True)
