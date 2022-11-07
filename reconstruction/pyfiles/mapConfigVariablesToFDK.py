de65468d8323c3f0fcc97fa255fbf05f820a774e
Jiayong Zhang <jiayong.zhang@ge.com>
2022-11-07 12:03:42 -0500

    fixed the negative sign in recon center offset

--- reconstruction/pyfiles/mapConfigVariablesToFDK.py
+++ reconstruction/pyfiles/mapConfigVariablesToFDK.py
@@ -18,8 +18,8 @@ def mapConfigVariablesToFDK(cfg):
     centerOffset = deepcopy(cfg.recon.centerOffset)
     # Pass desired X as Y
     centerOffset[1] = deepcopy(cfg.recon.centerOffset[0])
-    # Pass desired Y as -X
-    centerOffset[0] = -deepcopy(cfg.recon.centerOffset[1])
+    # Pass desired Y as X
+    centerOffset[0] = deepcopy(cfg.recon.centerOffset[1])
 
     # The FDK recon seems to be using a "start view" rather than a "start angele".
     # This is a hack until that gets fixed.
