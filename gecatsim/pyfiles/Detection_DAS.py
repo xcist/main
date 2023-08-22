f18665fc2ece6ca40eeb22bf157143de86c88de2
Jiayong Zhang <jiayong.zhang@ge.com>
2023-08-22 16:41:29 -0400

    add support for row-dependent spectrum (heel effects) and gain factor

--- gecatsim/pyfiles/Detection_DAS.py
+++ gecatsim/pyfiles/Detection_DAS.py
@@ -2,11 +2,21 @@
 
 import numpy as np
 from gecatsim.pyfiles.CommonTools import *
+import sys
 #from catsim.pyfiles.Detection_Lag import Detection_Lag
 
-def Detection_DAS(viewIn, viewId, cfg):
+def Detection_DAS_gain(viewIn, viewId, cfg):
 
-    viewOut = viewIn*cfg.scanner.detectionGain
+    if hasattr(cfg.scanner.detectionGain, "__len__"):
+        if len(cfg.scanner.detectionGain) != cfg.scanner.detectorRowCount:
+            print("Error! detectionGain should have the same length as row count\n")
+            sys.exit(1)
+        else:
+            # based on plot, viewIn is flatten of [col, row]
+            tmp = np.tile(cfg.scanner.detectionGain, cfg.scanner.detectorColCount)
+            viewOut = np.float32(viewIn*tmp)
+    else:
+        viewOut = viewIn*cfg.scanner.detectionGain
     
     eNoise = np.float32(np.random.randn(viewIn.size)*cfg.sim.eNoise)
     viewOut += eNoise.reshape(viewIn.shape)
