67e661df513e6c2d08adaafab63ec9c2024029b6
DEBNATH <223072889@ge.com>
2023-09-14 07:42:35 +0530

    unittest for ReadMaterialFile.py updated

--- gecatsim/tests/test_catsim/test_ReadMaterialFile.py
+++ gecatsim/tests/test_catsim/test_ReadMaterialFile.py
@@ -3,11 +3,10 @@ from gecatsim.pyfiles.ReadMaterialFile import ReadMaterialFile
 import unittest.mock
 
 class test_ReadMaterialFile(unittest.TestCase):
-    def Test_ReadMaterialFile(self):
-        (numberOfElements, density, atomicNumbers, massFractions) = ReadMaterialFile('../material/water')
-        print(numberOfElements, density, atomicNumbers, massFractions)
+    def test_ReadMaterialFile(self):
+        (numberOfElements, density, atomicNumbers, massFractions) = ReadMaterialFile('../../material/water')
 
         assert numberOfElements == 2
         assert density == 1.0
-        assert (atomicNumbers == [1, 8]).all()
-        assert massFractions == [0.111902, 0.888098].all()
+        assert atomicNumbers == [1, 8]
+        assert massFractions == [0.111902, 0.888098]
