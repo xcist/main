62cb9fe512ac2776c1470e01f74cf6e6a6d93577
Debnath <223072889@ge.com>
2024-02-15 13:56:21 +0530

    no changes made

--- gecatsim/tests/test_recon/test_recon.py
+++ gecatsim/tests/test_recon/test_recon.py
@@ -11,32 +11,27 @@ class Test_recon(unittest.TestCase):
         ct = xc.CatSim("../examples/cfg/Phantom_Sample", "../examples/cfg/Scanner_Sample_generic", "../examples/cfg/Protocol_Sample_axial")
 
         ##--------- Make changes to parameters (optional)
-        ct.resultsName = r"C:\Users\223072889\Desktop\CatSim\newCheckouts\main\gecatsim\tests\test_recon\test"
-        ct.protocol.viewsPerRotation = 1
+        ct.resultsName = "test"
+        ct.protocol.viewsPerRotation = 500
         ct.protocol.viewCount = ct.protocol.viewsPerRotation
         ct.protocol.stopViewId = ct.protocol.viewCount-1
         # ct.protocol.scanTypes = [1, 0, 0]  # flags for airscan, offset scan, phantom scan
         # ct.load_cfg("Protocol_Sample_axial", "Physics_Sample", "Recon_Sample_2d")  # new cfg overrides existing parameters
 
-        ct.protocol.mA = 800 
-        ct.scanner.detectorRowsPerMod = 4 
+        ct.protocol.mA = 800
+        ct.scanner.detectorRowsPerMod = 4
         ct.scanner.detectorRowCount = ct.scanner.detectorRowsPerMod
 
         ct.recon.fov = 300.0
         ct.recon.sliceCount = 4        # number of slices to reconstruct
         ct.recon.sliceThickness = 0.568  # reconstruction inter-slice interval (in mm)
 
-        print(ct.phantom.projectorCallback)
-       # sys.exit()
-
         ##--------- Run simulation
-        ct.run_all()  # run the scans defined by protocol.scanTypes
+        #ct.run_all()  # run the scans defined by protocol.scanTypes
 
         ##--------- Reconstruction
-        ct.do_Recon = 1 
-        #retval = recon.recon(ct)
+        ct.do_Recon = 1
+        retval = recon.recon(ct)
 
-        #assert(sys.getsizeof(retval)>0)
-        #assert(ct==retval)
-
-
+        assert(sys.getsizeof(retval)>0)
+        assert(ct==retval)
