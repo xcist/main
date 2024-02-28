5ad4e5ebcd8a13c01ccd4244d86bb465aa48422f
Reddy <212569890@ge.com>
2024-02-28 13:59:13 +0530

    Fixed testcases due to code changes in pull request 62.

--- gecatsim/tests/test_catsim/test_OneScan.py
+++ gecatsim/tests/test_catsim/test_OneScan.py
@@ -43,8 +43,10 @@ class TestOneScan(unittest.TestCase):
                     call(cfg.scanner.detectionCallback, cfg, 0, 0), call(cfg.physics.outputCallback, cfg, 0)]
         assert faval_mock.mock_calls == expected
 
+
+    @patch('gecatsim.pyfiles.PhantomProjectorWrapper.feval', create=True)
     @patch('gecatsim.pyfiles.OneScan.feval', create=True)
-    def test_phantom_scan(self, faval_mock):
+    def test_phantom_scan(self, onescan_feval_mock, phantom_projector_feval):
         cfg = CommonTools.CFG("../examples/cfg/Phantom_Sample", "../examples/cfg/Scanner_Sample_generic",
                               "../examples/cfg/Protocol_Sample_axial")
         cfg.sim.thisScanType = [0, 0, 1]
@@ -52,26 +54,29 @@ class TestOneScan(unittest.TestCase):
         cfg.protocol.stopViewId = 5
         # dummy values for the test to continue without failures
         cfg.detFlux = [[[1, 0, 0]], [[1, 0, 0]], [[1, 0, 0]]]
-        faval_mock.return_value = cfg
+        onescan_feval_mock.return_value = cfg
+        phantom_projector_feval.return_value = cfg
 
         o.one_scan(cfg)
 
-        expected = [call(cfg.phantom.callback, cfg),
-                    call(cfg.scanner.detectorCallback, cfg),
+        expected = [call(cfg.scanner.detectorCallback, cfg),
                     call(cfg.scanner.focalspotCallback, cfg),
                     call(cfg.physics.rayAngleCallback, cfg),
                     call(cfg.protocol.spectrumCallback, cfg),
                     call(cfg.protocol.filterCallback, cfg),
                     call(cfg.physics.fluxCallback, cfg)]
 
+        phantom_projector_feval_expected = [call(cfg.phantom.callback, cfg)]
+
         for viewId in range(cfg.protocol.stopViewId + 1):
-            for subViewId in range(2):
+            for subViewId in range(cfg.sim.subViewCount):
                 expected.append(call(cfg.protocol.scanTrajectory, cfg, viewId))
-                expected.append(call(cfg.phantom.projectorCallback, cfg, viewId, subViewId))
+                phantom_projector_feval_expected.append(call(cfg.phantom.projectorCallback, cfg, viewId, subViewId))
                 expected.append(call(cfg.scanner.detectionCallback, cfg, viewId, subViewId))
             expected.append(call(cfg.physics.outputCallback, cfg, viewId))
 
-        assert faval_mock.mock_calls == expected
+        assert onescan_feval_mock.mock_calls == expected
+        assert phantom_projector_feval.mock_calls == phantom_projector_feval_expected
 
     def test_update_scan_time(self):
         cfg = CommonTools.CFG("../examples/cfg/Phantom_Sample", "../examples/cfg/Scanner_Sample_generic",
