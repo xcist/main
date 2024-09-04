8862f6f78020d283f7be5825f9fa2b5ed0daff20
223072889 <anupama.debnath@gehealthcare.com>
2024-09-04 16:38:03 +0530

    Phantom_Analytic unittestcase updated

--- gecatsim/pyfiles/Phantom_Analytic.py
+++ gecatsim/pyfiles/Phantom_Analytic.py
@@ -915,16 +915,3 @@ def euler_angs(R=None):
     ea = [p1,p2,p3]
     
     return ea
-
-def Phantom_Polygonal_ReadPolygon(Verts):
-
-    ddir = my_path.find_dir("phantom", "poly_bin")
-    filename = 'poly{}'.format(Verts)
-    with open(os.path.join(ddir, filename),'rb') as fid:
-        data_array = np.fromfile(fid, dtype=np.int32, count=4)
-        nv_sz1, nv_sz2, vx_sz1, vx_sz2 = data_array
-        tmp = np.fromfile(fid, dtype=np.float64)
-        nV = tmp[:nv_sz1*nv_sz2].reshape(nv_sz2,nv_sz1).T
-        Vx = tmp[nv_sz1*nv_sz2:].reshape(vx_sz2,vx_sz1).T
-
-    return Vx,nV
--- gecatsim/tests/test_catsim/test_Phantom_Analytic.py
+++ gecatsim/tests/test_catsim/test_Phantom_Analytic.py
@@ -1,7 +1,7 @@
 from gecatsim.pyfiles.CommonTools import *
 import unittest.mock
 from gecatsim.pyfiles import CommonTools
-from gecatsim.pyfiles.Phantom_Analytic import Phantom_Analytic
+from gecatsim.pyfiles.Phantom_Analytic import Phantom_Analytic,Rmat, euler_angs, Phantom_Analytic_ConvertTori,get_clip_dD
 from unittest.mock import patch, MagicMock
 
 class Test_Phantom_Analytic(unittest.TestCase):
@@ -23,3 +23,28 @@ class Test_Phantom_Analytic(unittest.TestCase):
 
         assert cfg.clib.set_material_info.call_count == 1
         assert cfg.clib.set_phantom_info.call_count == 1
+
+    def test_get_clip_dD(self):
+        c0 = [0, 0, 0]
+        c1 = [0, 0, 0]
+        result = get_clip_dD(c0, c1)
+        expected = 0.0
+        self.assertEqual(result, expected)
+    def test_Rmat(self):
+        p = [30, 45, 60]
+        expected_output = np.array([
+            [0.12682648, 0.9267767, 0.35355339],
+            [-0.78033009, -0.12682648, 0.61237244],
+            [0.61237244, -0.35355339, 0.70710678]
+        ])
+        np.testing.assert_almost_equal(Rmat(p), expected_output, decimal=6)
+
+    def test_euler_angs(self):
+        R = np.array([
+            [0.36, -0.48, 0.8],
+            [0.8, 0.6, 0],
+            [-0.48, 0.64, 0.6]
+        ])
+        expected_ea = [np.arctan2(0.8, 0.6), np.arccos(0.6), np.arctan2(-0.48, 0.64)]
+        result = euler_angs(R)
+        np.testing.assert_almost_equal(result, expected_ea, decimal=5)
