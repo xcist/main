cdcb8fb3f4587d5275be73326325eb982587a037
nandanreddyl <130423440+nandanreddyl@users.noreply.github.com>
2024-09-18 12:05:25 +0530

    Update functional_test_disabled.py

--- gecatsim/tests/test_functional/functional_test_disabled.py
+++ gecatsim/tests/test_functional/functional_test_disabled.py
@@ -4,5 +4,5 @@ import os
 def functional_test_disabled():
     if os.getenv("ENABLE_FUNCTIONAL_TEST", "TRUE") == "FALSE":
         print("Functional Test Disabled")
-        return False
-    return True
+        return True
+    return False
