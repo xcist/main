5c204fe67a7554ee1c3ca308816f1738c41e078f
nandanreddyl <130423440+nandanreddyl@users.noreply.github.com>
2024-09-18 12:03:48 +0530

    Update functional_test_disabled.py

--- gecatsim/tests/test_functional/functional_test_disabled.py
+++ gecatsim/tests/test_functional/functional_test_disabled.py
@@ -1,1 +1,8 @@
+import os
 
+
+def functional_test_disabled():
+    if os.getenv("ENABLE_FUNCTIONAL_TEST", "TRUE") == "FALSE":
+        print("Functional Test Disabled")
+        return False
+    return True
