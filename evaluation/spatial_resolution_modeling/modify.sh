b8f4a553f0a0db685efd1f7533bb3801d9256d0d
Jiayong Zhang <jiayong.zhang@gehealthcare.com>
2024-11-04 14:58:34 -0500

    default enoise/gain to nominal values

--- evaluation/spatial_resolution_modeling/modify.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# gain factor
-sed -i "s/scanner.detectionGain.*/scanner.detectionGain = 10/g" */*/*/*cfg
-sed -i "s/scanner.detectionGain.*/scanner.detectionGain = 10/g" */*/*/*/*cfg
-# enoise
-sed -i "s/scanner.eNoise.*/scanner.eNoise = 1000/g" */*/*/*cfg
-sed -i "s/scanner.eNoise.*/scanner.eNoise = 1000/g" */*/*/*/*cfg
