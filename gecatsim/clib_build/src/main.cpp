ec3f4ba94509f9fe9d79111c332bf1943930fa1c
Jiayong Zhang <jiayong.zhang@gehealthcare.com>
2025-08-08 04:06:47 +0000

    add missing code

--- gecatsim/clib_build/src/main.cpp
+++ gecatsim/clib_build/src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2020, General Electric Company. All rights reserved. See https://github.com/xcist/code/blob/master/LICENSE
 
 #include "BaseObject.h"
+#include "TreePhantom.h"
 #include "MatVec.h"
 #include "CrossSection.hpp"
 #include "CrossSectionHandler.hpp"
@@ -27,8 +28,10 @@ extern "C" {
 #endif
 
 extern FILE *phantomin;
+extern LinearPhantom parsePhantomDefFile(double scaleFactor, MaterialTable &mtab);
 extern void printMaterialTable(FILE *fp);
 
+LinearPhantom phant;
 MaterialTable mtab;
 float* srcpos = NULL;
 float* detpos = NULL;
@@ -248,6 +251,44 @@ extern "C" {
     strcpy(matname,mtab[index].c_str());
   }
  
+  EXPORT void TranslatePhantom_FORBILD_to_tmp(double scalefact, char *filename_in, char *filename_out) {
+
+    FILE *fp = fopen(filename_in,"r");
+
+    if (!fp) {
+      std::cerr << "ERROR: unable to open " << filename_in << " for parsing...\r\n";
+      return;
+    }
+    phantomin = fp;
+	int result1 = ftell(fp);
+	int result2 = ftell(phantomin);
+
+    phant = parsePhantomDefFile(scalefact,mtab);
+    printf("    Found a total of %d objects\r\n", (int)phant.size());
+    fclose(fp);
+
+    fp = fopen(filename_out,"w");
+    fprintf(fp,"#################### MATERIAL TABLE START ####################\n");
+    printMaterialTable(fp);
+    fprintf(fp,"#################### MATERIAL TABLE END ######################\n");
+    std::vector<BaseObject*>::iterator myIter;
+    BaseObject* myObjectPtr;
+    
+    for(myIter=phant.begin();myIter!=phant.end();myIter++)
+      {
+	myObjectPtr = *myIter;
+	ostringstream stream1;
+	const char *ch;
+	string string1;
+	myObjectPtr->PrintMe(stream1);
+	string1 = stream1.str();
+	//cout << string1;
+	ch=string1.data();
+	fprintf(fp,"%s\n",ch);
+      }
+    fclose(fp);
+  }
+
   EXPORT void SetupDiscretePhantom(int pixels, int slices, float voxelsize, int numZ, int *zlist) {
     if (DiscretePhantom) delete DiscretePhantom ;
     DiscretePhantom = new Phantom;
