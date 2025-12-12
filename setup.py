9083d8e8080a2e0c85cba75403b3d7c940d3bc64
Jiayong Zhang <jiayong.zhang@gehealthcare.com>
2025-12-12 15:46:32 +0000

    add support for local compilation using pip install

--- setup.py
+++ setup.py
@@ -3,10 +3,38 @@
 # To install XCIST-CatSim, open Python console, run: pip install [folder name]
 # e.g., you can navigate to this folder, run: pip install .
 
-from setuptools import setup
+from setuptools import setup, Extension
+from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.build_ext import build_ext
+import subprocess
+import os
+import sys
+
+
+class BuildAssetsCommand(build_ext):
+    """Run a bash script before building Python modules."""
+    def run(self):
+        # Only run if the script exists (useful when sdist excludes it)
+        script = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build", "BuildLinux64")
+        print(script)
+        if os.path.exists(script):
+            print("Building the catsim library with: ", script)
+            if sys.platform.startswith("win"):
+                # Prefer cross-platform steps written in Python,
+                # but if you must, call via bash provided by Git-Bash/MSYS2.
+                bash = os.environ.get("BASH", "bash")
+                subprocess.check_call([bash, script])
+            else:
+                subprocess.check_call(["bash", script], cwd = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build"))
+        else:
+            print("Error! script not found!")
+        # Continue with normal build
+        super().run()
 
 setup(name='gecatsim',
       version='1.6.5',
+      ext_modules = [Extension("dummy", sources=[])],
+      cmdclass = {"build_ext": BuildAssetsCommand},
       description='Simulation toolkit for X-ray based cancer imaging',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
@@ -24,6 +52,7 @@ setup(name='gecatsim',
                     r'examples/*.*', r'examples/cfg/*.*', r'examples/vct_examples/*.*',
                     r'focal_spot/*.*',
                     r'lib/*.*',
+                    r'clib_build/*', r'clib_build/*/*', r'clib_build/*/*/*',
                     r'material/*', r'material/edlp/*/*.dat',
                     r'phantom/*.*', r'phantom/CatSimLogo_1024/*.*', r'phantom/poly_bin/poly*',
                     r'pyfiles/FlatPanel/*.*',
