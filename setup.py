44bceb5173f890e2bb76bba2a2711e8c9bf2d4b2
J. Jobin <jjobin@gmail.com>
2025-12-17 12:38:29 -0800

    New PyPI release.

--- setup.py
+++ setup.py
@@ -15,24 +15,25 @@ class BuildAssetsCommand(build_ext):
     """Run a bash script before building Python modules."""
     def run(self):
         # Only run if the script exists (useful when sdist excludes it)
-        script = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build", "BuildLinux64")
-        print(script)
-        if os.path.exists(script):
-            print("Building the catsim library with: ", script)
-            if sys.platform.startswith("win"):
-                # Prefer cross-platform steps written in Python,
-                # but if you must, call via bash provided by Git-Bash/MSYS2.
-                bash = os.environ.get("BASH", "bash")
-                subprocess.check_call([bash, script])
+        if sys.platform == 'linux':
+            script = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build", "BuildLinux64")
+            print(script)
+            if os.path.exists(script):
+                print("Building the catsim library with: ", script)
+                if sys.platform.startswith("win"):
+                    # Prefer cross-platform steps written in Python,
+                    # but if you must, call via bash provided by Git-Bash/MSYS2.
+                    bash = os.environ.get("BASH", "bash")
+                    subprocess.check_call([bash, script])
+                else:
+                    subprocess.check_call(["bash", script], cwd = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build"))
             else:
-                subprocess.check_call(["bash", script], cwd = os.path.join(os.path.dirname(__file__), "gecatsim", "clib_build"))
-        else:
-            print("Error! script not found!")
-        # Continue with normal build
-        super().run()
+                print("Error! script not found!")
+            # Continue with normal build
+            super().run()
 
 setup(name='gecatsim',
-      version='1.6.6',
+      version='1.6.8',
       ext_modules = [Extension("dummy", sources=[])],
       cmdclass = {"build_ext": BuildAssetsCommand},
       description='Simulation toolkit for X-ray based cancer imaging',
